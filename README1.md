c990e489153eeb1576c9f0229e4e75260de20413
Lucky <sashankaswain6@gmail.com>
2025-05-08 20:16:24 +0530

    adding node mailer

--- README1.md
+++ README1.md
@@ -3,3 +3,21 @@ then go to src folder
        `cd src`
 then do `npx sequelize init --force`
 then do `npx sequelize db:migrate`
+
+
+{
+        what is notification service
+        now create a new model
+        create a new model called `Notification` in the `models` folder
+        create a new file called `notification.js` in the `models` folder
+        add the following code to the `notification.js` file
+        `npx sequelize model:generate --name Ticket --attributes subject:string,content:string,recepientEmail:string,status:string`
+}
+
+{
+        for notification service we use a new email
+        use nodemailer
+        run
+        `npm i nodemailer`
+        
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-const express = require('express');
+const express = require("express");
 
-const { ServerConfig } = require('./config');
-const apiRoutes = require('./routes');
+const { ServerConfig } = require("./config");
+const apiRoutes = require("./routes");
+
+const mailsender = require("./config/email-config");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
+ try {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    const response = await mailsender.sendMail({
+      from: ServerConfig.GMAIL_EMAIL,
+      to: "abc868@gmail.com",
+      subject: "Server Started ? now as well now ",
+      text: "Server is running successfully",
+    });
+  
+    console.log(response);
+ } catch (error) {
+    console.log(error);
+    throw error;
+ }
+
 });
